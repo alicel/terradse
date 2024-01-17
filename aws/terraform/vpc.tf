afdf03a61e11c367abbc19addcddf7d484ecd76d
Alice Lottini <alice.lottini@datastax.com>
2024-01-17 12:24:17 +0000

    Update vpc.tf

--- aws/terraform/vpc.tf
+++ aws/terraform/vpc.tf
@@ -81,23 +81,6 @@ resource "aws_route_table_association" "rt_assoc_sn_dse_cassapp" {
     subnet_id               = aws_subnet.sn_dse_cassapp.id
 }
 
-# Subnet for DSE advanced workloads - application cluster
-/*
-resource "aws_subnet" "sn_dse_solrspark" {    
-    vpc_id                  = aws_vpc.vpc_dse.id
-    cidr_block              = var.vpc_cidr_str_solrspark
-    map_public_ip_on_launch = true
-
-    tags = {
-        Name = "${var.tag_identifier}-sn_dse_solrspark"
-    }
-}
-resource "aws_route_table_association" "rt_assoc_sn_dse_solrspark" {
-    route_table_id          = aws_route_table.rt_dse.id
-    subnet_id               = aws_subnet.sn_dse_solrspark.id
-}
-*/
-
 # Subnet for user application client
 resource "aws_subnet" "sn_user_app" {
   vpc_id                  = aws_vpc.vpc_dse.id
