4fd5198fa6ad1315c9d4ab9f4a0afd005f8a213d
Alice Lottini <alice.lottini@datastax.com>
2024-01-17 21:13:06 +0000

    Tweaks to the app client install playbook and shell script. Some fixes

--- aws/init_running_app_clients.sh
+++ aws/init_running_app_clients.sh
@@ -53,9 +53,9 @@ do
 done
 
 
-# Login using the ZDM Immersion Day profile
-printf "Logging in the AWS account using the ZDM Immersion Day profile - please click on Allow when prompted\n\n"
-aws sso login --profile ds-zdm-immersion-day
+# Login using the Astra Conn profile
+printf "Logging in the AWS account using the Astra Conn profile - please click on Allow when prompted\n\n"
+aws sso login --profile astra-conn
 
 printf "\n\nCreating the app client inventory file...\n"
 printf "[clients]\n" > ansible/app_client_inventory
@@ -65,12 +65,35 @@ printf "[clients]\n" > ansible/app_client_inventory
 aws ec2 describe-instances \
  --filter "Name=tag:UsageGroup,Values=client" "Name=instance-state-name,Values=running" \
  --query "Reservations[*].Instances[*].[PublicIpAddress]" \
- --profile=ds-zdm-immersion-day \
- --region=us-west-2 \
+ --profile=astra-conn \
+ --region=us-east-1 \
  --output text >> ansible/app_client_inventory
 
+# Retrieve the private IP of cluster node0
+DSE0_IP=$(aws ec2 describe-instances \
+                --filter "Name=tag:UsageGroup,Values=cluster" "Name=tag:Name,Values=origin-sideloader-dc1-node0" \
+                --query "Reservations[*].Instances[*].[PrivateIpAddress]" \
+                --profile=astra-conn \
+                --region=us-east-1 \
+                --output text)
+
+# Retrieve the private IP of cluster node1
+DSE1_IP=$(aws ec2 describe-instances \
+ --filter "Name=tag:UsageGroup,Values=cluster" "Name=tag:Name,Values=origin-sideloader-dc1-node1" \
+ --query "Reservations[*].Instances[*].[PrivateIpAddress]" \
+ --profile=astra-conn \
+ --region=us-east-1 \
+ --output text)
+
+# Retrieve the private IP of cluster node2
+DSE2_IP=$(aws ec2 describe-instances \
+ --filter "Name=tag:UsageGroup,Values=cluster" "Name=tag:Name,Values=origin-sideloader-dc1-node2" \
+ --query "Reservations[*].Instances[*].[PrivateIpAddress]" \
+ --profile=astra-conn \
+ --region=us-east-1 \
+ --output text)
 printf "\nClient inventory file created\n"
 
 printf "\nRunning the client install playbook now\n\n"
 cd ansible || return
-ansible-playbook app_clients_install.yaml -u ubuntu --private-key "$ORIGIN_SSH_KEY" -i app_client_inventory
+ansible-playbook app_clients_install.yaml -u ubuntu --private-key "$ORIGIN_SSH_KEY" --extra-vars "dse0_priv_ip=$DSE0_IP dse1_priv_ip=$DSE1_IP dse2_priv_ip=$DSE2_IP" -i app_client_inventory
